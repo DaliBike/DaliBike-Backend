469d07d29f3c7b9d7c4450f504db7d251d977231
Meowning <118808242+Meowning@users.noreply.github.com>
2024-05-14 18:34:58 +0900

    view 파일 변경

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
