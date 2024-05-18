8149ab4c45a83737f3e68eaf2d75545d475c9161
Meowning <118808242+Meowning@users.noreply.github.com>
2024-05-18 14:13:42 +0900

    edit result query

--- model/map.js
+++ model/map.js
@@ -6,64 +6,64 @@ const mysql = require('./config.js');
 const map = {
     getAirInjectorList : async function() {
         try {
-            const [result] = await mysql.query("SELECT AIId, Latitude, Longitude FROM AirInjector");
-            return [result];
+            const result = await mysql.query("SELECT AIId, Latitude, Longitude FROM AirInjector");
+            return result;
         } catch (error) {
             console.log("map: airInjector list 조회 오류 발생");
         }
     },
     getStorageList : async function(id) {
         try {
-            const [result] = await mysql.query("SELECT SFId, Latitude, Longitude  FROM StorageFacility");
-            return [result];
+            const result = await mysql.query("SELECT SFId, Latitude, Longitude  FROM StorageFacility");
+            return result;
         } catch (error) {
             console.log("map: storageFacility list 조회 오류 발생");
         }
     },
     getStoreList : async function() {
         try {
-            const [result] = await mysql.query("SELECT StoreId, Latitude, Longitude FROM Store");
-            return [result];
+            const result = await mysql.query("SELECT StoreId, Latitude, Longitude FROM Store");
+            return result;
         } catch (error) {
             console.log("map: store list 조회 오류 발생");
         }
     },
     getStoreDetails : async function(id) {
         try {
-            const [result] = await mysql.query("SELECT StoreName, StorePhone, LocalAddress, RoadAddress, DayOff, StartTime, EndTime FROM Store WHERE StoreId = ?", [id]);
-            return [result];
+            const result = await mysql.query("SELECT StoreName, StorePhone, LocalAddress, RoadAddress, DayOff, StartTime, EndTime FROM Store WHERE StoreId = ?", [id]);
+            return result;
         } catch (error) {
             console.log("map: store detail 조회 오류 발생");
         }
     },
     getRentalStationList : async function() {
         try {
-            const [result] = await mysql.query("SELECT RSId, Latitude, Longitude FROM RentalStation");
-            return [result];
+            const result = await mysql.query("SELECT RSId, Latitude, Longitude FROM RentalStation");
+            return result;
         } catch (error) {
             console.log("map: rental station list 조회 오류 발생");
         }
     },
     getRentalStationDetails : async function(id) {
         try {
-            const [result] = await mysql.query("SELECT RSName, UnmanRS, ManRS, RoadAddress, LocalAddress, StartTime, EndTime, DayOff, IsFare, Fare, ManagePhone FROM RentalStation WHERE RSId = ?", [id]);
-            return [result];
+            const result = await mysql.query("SELECT RSName, UnmanRS, ManRS, RoadAddress, LocalAddress, StartTime, EndTime, DayOff, IsFare, Fare, ManagePhone FROM RentalStation WHERE RSId = ?", [id]);
+            return result;
         } catch (error) {
             console.log("map: rental station detail 조회 오류 발생");
         }
     },
     getLodgingList : async function() {
         try {
-            const [result] = await mysql.query("SELECT LodgingId, Latitude, Longitude FROM Lodging");
-            return [result];
+            const result = await mysql.query("SELECT LodgingId, Latitude, Longitude FROM Lodging");
+            return result;
         } catch (error) {
             console.log("map: lodging list 조회 오류 발생");
         }
     },
     getLodgingDetails : async function(id) {
         try {
-            const [result] = await mysql.query("SELECT BusinessName, LocationPhoneNumber, LocalAddress, LocationPostcode, RoadAddress, RoadPostcode FROM Lodging WHERE LodgingId = ?", [id]);
-            return [result];
+            const result = await mysql.query("SELECT BusinessName, LocationPhoneNumber, LocalAddress, LocationPostcode, RoadAddress, RoadPostcode FROM Lodging WHERE LodgingId = ?", [id]);
+            return result;
         } catch (error) {
             console.log("map: lodging details 조회 오류 발생");
         }
