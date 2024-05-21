f0b4341b350eb2fea49c497b363e8446852c902e
Meowning <118808242+Meowning@users.noreply.github.com>
2024-05-21 21:36:25 +0900

    asdf

---
+++ routers/report.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const reportController = require("../controller/report");
+
+//router.post("/add", reportController.addReport);
+//router.post("/save/image", reportController.saveImage);
+
+module.exports = router;
