2cf1c95f7982a39465d5947be683882e6a963dbf
Meowning <118808242+Meowning@users.noreply.github.com>
2024-06-03 00:28:00 +0900

    edit record

--- controller/record.js
+++ controller/record.js
@@ -4,7 +4,7 @@ const recordController = {
     viewToday: async (req, res) => {
         try {
             const {id} = req.body;
-            const result = await record.viewToday();
+            const result = await record.viewToday(id);
             res.json(result);
         } catch (err) {
             console.log("record: 오늘 기록 조회 컨트롤러 오류 : " + err)
@@ -14,7 +14,7 @@ const recordController = {
     viewMonthly: async (req, res) => {
         try {
             const {id, month} = req.body;
-            const result = await record.viewMonthly();
+            const result = await record.viewMonthly(id, month);
             res.json(result);
         } catch (err) {
             console.log("record: 월별 기록 조회 컨트롤러 오류")
@@ -24,7 +24,7 @@ const recordController = {
     viewRank: async (req, res) => {
         try {
             const {month} = req.body;
-            const result = await record.viewMonthly();
+            const result = await record.viewMonthly(month);
             res.json(result);
         } catch (err) {
             console.log("record: 월별 기록 조회 컨트롤러 오류")
@@ -34,7 +34,7 @@ const recordController = {
     viewMyRank: async (req, res) => {
         try {
             const {id, month} = req.body;
-            const result = await record.viewMyRank();
+            const result = await record.viewMyRank(id, month);
             res.json(result);
         } catch (err) {
             console.log("record: 내 랭킹 조회 컨트롤러 오류")
@@ -44,7 +44,7 @@ const recordController = {
     record: async (req, res) => {
         try {
             const {id, dailyTime} = req.body;
-            const result = await record.record();
+            const result = await record.record(id, dailyTime);
             res.json(result);
         } catch (err) {
             console.log("record: 기록 컨트롤러 오류")
