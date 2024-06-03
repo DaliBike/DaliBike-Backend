029d5d99a443e706034548e70373502ebeb1e72a
LNK <lwknk1119@naver.com>
2024-06-04 00:29:13 +0900

    modfiy [record record return]

--- model/record.js
+++ model/record.js
@@ -4,7 +4,7 @@ const mysql = require('./config.js');
 
 
 const record = {
-    viewToday: async function(id) {
+    viewToday: async function (id) {
         try {
             const [result] = await mysql.query("SELECT * FROM record WHERE USERId = ? AND date = CURDATE()", [id]);
             return result;
@@ -13,19 +13,19 @@ const record = {
         }
     },
 
-    viewMonthly : async function(id, year, month) {
+    viewMonthly: async function (id, year, month) {
         try {
             const [result] = await mysql.query(
                 "SELECT * FROM record WHERE USERId = ? AND date BETWEEN ? AND ? ORDER BY date ASC",
                 [id, `${year}-${month}-01`, `${year}-${month}-31`]
-              );
+            );
             return result;
         } catch (error) {
             console.log("record: 월별 기록 조회 오류 발생");
         }
     },
 
-    viewRank : async function(year, month) {
+    viewRank: async function (year, month) {
         try {
             const [result] = await mysql.query(
                 "SELECT USERId, SUM(dailyTime) AS totalTime FROM record WHERE date BETWEEN ? AND ? GROUP BY USERId ORDER BY totalTime DESC LIMIT 3",
@@ -37,14 +37,14 @@ const record = {
         }
     },
 
-    viewMyRank: async function(id, year, month) {
+    viewMyRank: async function (id, year, month) {
         try {
             const [result] = await mysql.query(
                 `SELECT USERId, SUM(dailyTime) AS totalTime, 
                         RANK() OVER (ORDER BY SUM(dailyTime) DESC) as rank 
-                 FROM record 
-                 WHERE date BETWEEN ? AND ? 
-                 GROUP BY USERId`,
+                FROM record 
+                WHERE date BETWEEN ? AND ? 
+                GROUP BY USERId`,
                 [`${year}-${month}-01`, `${year}-${month}-31`]
             );
             const userRecord = result.find(record => record.USERId === id);
@@ -58,9 +58,9 @@ const record = {
             throw error;
         }
     },
-    
 
-record: async function(id, dailyTime) {
+
+    record: async function (id, dailyTime) {
         try {
             const todayRecord = await this.viewToday(id);
             if (todayRecord.length === 0) {
@@ -69,9 +69,11 @@ record: async function(id, dailyTime) {
                 const existDailyTime = todayRecord[0].dailyTime;
                 await mysql.query("UPDATE record SET dailyTime = ? WHERE USERId = ? AND date = DATE_SUB(NOW(), INTERVAL ? SECOND)", [existDailyTime + dailyTime, dailyTime, id]);
             }
+            return true;
         } catch (error) {
             console.log("record: 기록 오류 발생" + error);
             throw error;
+            return false;
         }
     }
 }
