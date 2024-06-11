5959e1b2f9f1ad248d901c577fec4503e90d962e
gam1017 <gam1017@kumoh.ac.kr>
2024-06-11 22:35:20 +0900

    OMG

--- routers/post.js
+++ routers/post.js
@@ -12,4 +12,4 @@ router.post("/upload", postController.InsertPost)   // 게시글 등록
 router.delete("/delete/:postId", postController.DeletePost) // 게시글 삭제
 router.post("/like", postController.LikePost) // 게시글 좋아요
 
-module.exports = router;n
+module.exports = router;
