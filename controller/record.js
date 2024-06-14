3b58aa8eb846eb71b934c4702379b5c65087dc4d
gam1017 <gam1017@kumoh.ac.kr>
2024-06-14 19:59:22 +0900

    change

--- controller/record.js
+++ controller/record.js
@@ -17,7 +17,7 @@ const recordController = {
         try {
             const {id, year, month} = req.body;
             const result = await record.viewMonthly(id, year, month);
-            if (result.length === 0)   res.json({"id": id, "dailyTime": 0}) // 월간 기록이 아예 없는 경우 이거만 던짐
+            if (result.length === 0)   res.json([{"id": id, "dailyTime": 0}]) // 월간 기록이 아예 없는 경우 이거만 던짐
             else                        res.json(result);
         } catch (err) {
             console.log("record: 월별 기록 조회 컨트롤러 오류")
@@ -29,8 +29,8 @@ const recordController = {
         try {
             const {year, month} = req.body;
             const result = await record.viewRank(year, month);
-            if (result.length === 0)    res.json({"id": 0, "totalTime": 0})
-            res.json(result);
+            if (result.length === 0)    res.json([{"USERId": 0, "totalTime": 0}])
+            else                        res.json(result);
         } catch (err) {
             console.log("record: 월별 기록 조회 컨트롤러 오류")
             res.json({ "result": "error" });
