5df906616fd77c9acf64b6f896fad14eb8c8488a
Meowning <118808242+Meowning@users.noreply.github.com>
2024-06-15 19:49:27 +0900

    edit dbconfig

--- model/config.js
+++ model/config.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-    host: 'db.meowning.kr',
+    host: 'localhost',
     port: 3306,
     user: 'dalibike',
     password: 'dalibike',
