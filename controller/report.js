54eb1dffbab2c9f0e48bbd515c8ee227ea648151
Meowning <118808242+Meowning@users.noreply.github.com>
2024-06-18 06:54:42 +0900

    ㄹㄴㅇ호

--- controller/report.js
+++ controller/report.js
@@ -24,23 +24,18 @@ const reportController = {
             }
             else {
                 if (!isAvailable1) {
-                    res.status(400).json({ "result": "can't report twice" });
-                    throw new Error("한 유저가 같은 위치의 요소를 두 번 이상 신고할 수 없습니다.");
+                    res.status(200).json({ "result": "can't report twice" });
                 }
                 else {
-                    res.status(400).json({ "result": "approved report exists" });
-                    throw new Error("주변에 이미 승인된 제보가 있습니다.");
+                    res.status(200).json({ "result": "approved report exists" });
                 }
             }
         } catch (error) {
             console.log("report: addReport controller 오류 발생" + error);
             if (error.message !== "이미지가 없습니다.") {
                 await report.deleteImage(req.file.path);
-            }
-            if (error.message !== "한 유저가 같은 위치의 요소를 두 번 이상 신고할 수 없습니다."
-                && error.message !== "주변에 이미 승인된 제보가 있습니다.") {            
-                res.status(500).json({ "result": "server error" });
-            }
+            }         
+            res.status(500).json({ "result": "server error" });
         }
     },
     addReportRemoval : async function(req, res) {
@@ -62,17 +57,14 @@ const reportController = {
                 }
             }
             else {
-                res.status(400).json({ "result": "can't report twice" });
-                throw new Error("한 유저가 같은 위치의 요소를 두 번 이상 신고할 수 없습니다.");
+                res.status(200).json({ "result": "can't report twice" });
             }
         } catch (error) {
             console.log("report: addReportRemoval controller 오류 발생" + error);
             if (error.message !== "이미지가 없습니다.") {
                 await report.deleteImage(req.file.path);
             }
-            if (error.message !== "한 유저가 같은 위치의 요소를 두 번 이상 신고할 수 없습니다.") {            
-                res.status(500).json({ "result": "server error" })
-            }
+            res.status(500).json({ "result": "server error" })
         }
     },
     getReportDetails : async function(req, res) {
